24a65c059ffea981764b7ba36f19ee3710b46e69
Nords Zamora <100557534+fafsnords@users.noreply.github.com>
2023-06-02 13:56:18 -1200

    Update settings.py

--- artwork/artwork/settings.py
+++ artwork/artwork/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-%(-*y)8@y^y_+qh+pqt(7pk)!q8z06uiu5dj2f7f-sh-xqlxu_
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['.vercel.app']
+ALLOWED_HOSTS = ['127.0.0.1', '.vercel.app']
 
 
 # Application definition
