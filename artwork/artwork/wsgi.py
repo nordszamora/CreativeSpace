8de981e2d584b61c45b41dcd54c59d049788a4ba
Nords Zamora <100557534+fafsnords@users.noreply.github.com>
2023-06-02 13:59:59 -1200

    Update wsgi.py

--- artwork/artwork/wsgi.py
+++ artwork/artwork/wsgi.py
@@ -14,10 +14,13 @@ from django.core.wsgi import get_wsgi_application
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'artwork.settings')
 
 application = get_wsgi_application()
-'''
+
 
 import os
 from django.core.wsgi import get_wsgi_application
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "artwork.settings")
 application = get_wsgi_application()
 app = application
+'''
+
+app = get_wsgi_application()
